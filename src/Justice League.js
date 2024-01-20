644b78c9d58c5d6ce2cc8f532ade418214dd12d4
johnDaro7 <153467716+johnDaro7@users.noreply.github.com>
2024-01-20 21:23:41 +0100

    Add files via upload
    Tnx

---
+++ src/Justice League.js
@@ -0,0 +1,106 @@
+export const characters = [
+  // 
+  {
+    name: "Clark Kent, Kal-El",
+    alias: "Super Man",
+    img1: "http://m.gettywallpapers.com/wp-content/uploads/2021/04/Black-Superman-Wallpapers-1024x576.jpg",
+    homeworld: "planet Krypton",
+    abilities: [
+      "Super Speed",
+      "flight",
+      "X-ray vision",
+      "Super strength",
+      "freeze breath",
+      "superhuman hearing",
+      "invulnerability",
+      "healing factor",
+    ],
+    team: "Justice League",
+    description:
+      "Clerk Kent, also known as Super Man, Faster than a speeding bullet, more powerful than a locomotive… The Man of Steel fights a never-ending battle for truth, justice, and the American way.",
+  },
+  {
+    name: "Diana Prince",
+    alias: "Wonder woman",
+    abilities: ["super strength", "invulnerability", "flight", "combat skill",
+    "combat strategy", "superhuman agility", "healing factor", "magic weaponry"],
+    team: "Justice League",
+    img1: "http://m.gettywallpapers.com/wp-content/uploads/2023/07/Wonder-Woman-Pofile-Picture-HD.jpg",
+    homeworld: "Paradise Island",
+    description:
+      "Beautiful as Aphrodite, wise as Athena, swifter than Hermes, and stronger than Hercules, Princess Diana of Themyscira fights for peace in Man's World.",
+  },
+  {
+    name: "Bruce Wayne",
+    alias: "BatMan",
+    abilities: ["exceptional martial artist", "combat strategy", "inexhaustible wealth", 
+    "brilliant deductive skill", "advanced technology"],
+    team: "Jusicce League",
+    img1: "http://m.gettywallpapers.com/wp-content/uploads/2023/05/Batman-Dark-Knight-Android-Wallpaper.jpg",
+    homeworld: "Planet Earth",
+
+    description:
+      "In the name of his murdered parents, Bruce Wayne wages eternal war on the criminals of Gotham City. He is vengeance. He is the night. He is Batman.",
+  },
+  {
+    name: "Barry Allen",
+    alias: "The Flash",
+    abilities: ["super speed", "intangibility", "superhuman agility", "time travel", 
+    "creates and controls lightning", "multiversal knowledge"],
+    team: "Justice League",
+    img1: "http://m.gettywallpapers.com/wp-content/uploads/2020/04/Flash-Wallpaper-Android.jpg",
+    homeworld: "Planet Earth",
+
+    description:
+      "Three men have held the title of The Fastest Man Alive—Jay Garrick, Barry Allen and Wally West. Each of them redefined the word hero.",
+  },
+
+  //
+  {
+    name: "Victor (Vic) Stone",
+    alias: "Cyborg",
+    abilities: ["super strength", "advanced technology", "instant weaponry", "genius-level intellect", 
+    "control over technology", "computer hacking", "durability", "teleportation"],
+    team: "Justice League",
+    img1: "https://images6.alphacoders.com/868/868447.jpg",
+    homeworld: "Planet Earth",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      "Part man, part machine, Vic Stone is a former member of the Teen Titans and a current member of the Justice League who wrestles to preserve his humanity with every new upgrade.",
+  },
+
+  {
+    name: "Arthur Curry",
+    alias: "AquaMan",
+    abilities: ["super strength", "durability", "control over sea life", "exceptional swimming ability",
+     "ability to breathe underwater"],
+    team: "Justice League",
+    img1: "http://m.gettywallpapers.com/wp-content/uploads/2022/03/Aquaman-Wallpaper-Pictures.jpg",
+    homeworld: "Planet Earth",
+    description:
+      "The son of a lighthouse keeper and an Atlantean queen, Arthur Curry is the bridge between the surface world and his tumultuous realm of the sea.",
+  },
+
+  {
+    name: "Oliver Queen",
+    alias: "Green Arrow",
+    abilities: ["archery", "unrivaled aim", "exceptional martial artist", "inexhaustible wealth"],
+    team: "justice League",
+    img1: "http://m.gettywallpapers.com/wp-content/uploads/2020/09/Arrow-Wallpaper-HD.jpg",
+    homeworld: "Planet Earth",
+    description:
+      "Billionaire Oliver Queen uses both his wealth and his unmatched archery skills as the Justice League's battling bowman, Green Arrow.",
+  },
+  {
+    name: "Hal Jordan",
+    alias: "Green Lantern",
+    abilities: ["hard light constructs", "instant weaponry", "force fields", "flight", "durability", "alien technology"],
+    team: "justice League",
+    img1: "https://images5.alphacoders.com/325/thumb-350-325142.jpg",
+    homeworld: "Planet MOGO",
+    description:
+      "Test pilot Hal Jordan went from being a novelty, the first-ever human Green Lantern, to one of the most legendary Lanterns to ever wield a power ring.",
+  },
+];
+
+console.log(characters);
